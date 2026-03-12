bfff6872f3bc755f2dc0f26154f04eaef2d45771
MikhayahIV <sasha.vessa@gmail.com>
2026-03-12 17:47:16 -0300

    feat: implement UserModel and ActivityModel with ManyToMany relationship

---
+++ CadastroDeUsuarios/src/main/java/jay/six/CadastroDeUsuarios/activity/model/ActivityModel.java
@@ -0,0 +1,78 @@
+package jay.six.CadastroDeUsuarios.activity.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jay.six.CadastroDeUsuarios.model.UserModel;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ACTIVITIES")
+public class ActivityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    private String name;
+
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name ="TB_USERS_ACTIVITIES",joinColumns = @JoinColumn(name = "activity_id"),inverseJoinColumns = @JoinColumn(name = "user_id"))
+    @JsonIgnoreProperties("activities")
+    private Set<UserModel> users = new HashSet<>();
+
+    public ActivityModel() {
+    }
+
+    public ActivityModel(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<UserModel> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserModel> users) {
+        this.users = users;
+    }
+
+    public void addUser(UserModel user){
+       this.users.add(user);
+       user.getActivity().add(this);
+    }
+
+    public void removeUsers(UserModel user){
+        this.users.remove(user);
+        user.getActivity().remove(this);
+    }
+
+}
---
+++ CadastroDeUsuarios/src/main/java/jay/six/CadastroDeUsuarios/model/UserModel.java
@@ -0,0 +1,122 @@
+package jay.six.CadastroDeUsuarios.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jay.six.CadastroDeUsuarios.activity.model.ActivityModel;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+
+    private String photo;
+
+    @Column(name = "birth_date",nullable = false)
+    private LocalDate birthDate;
+
+    @ManyToMany(mappedBy = "users")
+    @JsonIgnoreProperties("users")
+    private Set<ActivityModel> activity = new HashSet<>();
+
+    public UserModel() {
+    }
+
+    public UserModel(LocalDate birthDate, String photo, String phone, String password, String email, String name) {
+        this.birthDate = birthDate;
+        this.photo = photo;
+        this.phone = phone;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Set<ActivityModel> getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Set<ActivityModel> activity) {
+        this.activity = activity;
+    }
+
+    public int getAge(){
+        if(this.birthDate ==null) return 0;
+        return Period.between(this.birthDate, LocalDate.now()).getYears();
+    }
+
+}
