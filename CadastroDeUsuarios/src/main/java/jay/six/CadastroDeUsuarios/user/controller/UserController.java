e8b721279579882529eb049d509ff5f7c9995606
MikhayahIV <sasha.vessa@gmail.com>
2026-03-15 19:12:09 -0300

    feat: implement remaining CRUD endpoints and standardize REST routes

--- CadastroDeUsuarios/src/main/java/jay/six/CadastroDeUsuarios/user/controller/UserController.java
+++ CadastroDeUsuarios/src/main/java/jay/six/CadastroDeUsuarios/user/controller/UserController.java
@@ -7,10 +7,9 @@ import jay.six.CadastroDeUsuarios.user.mapper.UserMapper;
 import jay.six.CadastroDeUsuarios.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("v1/users")
@@ -24,10 +23,32 @@ public class UserController {
         this.mapper = mapper;
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO dto){
-        UserResponseDTO user = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(user);
+                .body(service.create(dto));
     }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> allUsers(){
+        return ResponseEntity.ok(service.usersList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable UUID id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUserById(@PathVariable UUID id, @RequestBody @Valid UserRequestDTO dto){
+        return ResponseEntity.ok(service.attUser(id,dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable UUID id){
+        service.delete(id);
+        return ResponseEntity.noContent()
+                .build();
+    }
+
 }
